f1ad99780300cf7a6f42be68f376ef4ae7f39415
bunkahle <bunkahle@users.noreply.github.com>
2019-03-24 13:33:20 +0100

    Add files via upload

---
+++ tests/test_fonts2.py
@@ -0,0 +1,19 @@
+	from __future__ import print_function
+	import PILasOPENCV as Image
+	import PILasOPENCV as ImageDraw
+	import PILasOPENCV as ImageFont
+	import cv2
+
+	font = ImageFont.truetype("ARIAL.ttf".lower(), 18)
+	im = Image.open("lena.jpg")
+	draw = ImageDraw.Draw(im)
+	text = "Lena's image"
+	draw.text((249,455), text, font=font, fill=(0, 0, 0))
+	# in PIL:
+	# print(font.getsize(text))
+	# mask = font.getmask(text)
+	print(ImageFont.getsize(text, font))
+	mask = ImageFont.getmask(text, font)
+	print(type(mask))
+	cv2.imshow("mask", mask)
+	im.show()
