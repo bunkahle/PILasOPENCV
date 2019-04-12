c3abf21fd9b1c792fadc90d53b30afd4f4e99f86
bunkahle <bunkahle@users.noreply.github.com>
2019-04-13 00:23:48 +0200

    Add files via upload

--- tests/test_color.py
+++ tests/test_color.py
@@ -4,3 +4,8 @@ import PILasOPENCV as ImageMode
 
 print(dir(ImageColor))
 print(dir(ImageMode))
+
+colors = ["black", "white", "red", "orange", "yellow", "green", "blue", "violet", "magenta"]
+for color in colors:
+	print("Color:", color, ImageColor.ImageColor().getrgb(color))
+	print("Color:", color, ImageColor.ImageColor().getcolor(color, "L"))
